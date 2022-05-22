09345e3a3391167f19f97b5a63b1e5b1d3739884
Xintao <wxt1994@126.com>
2022-05-22 17:10:08 +0800

    update url

--- html/data/data.js
+++ html/data/data.js
@@ -3,14 +3,14 @@ var data = [
     "title": "Template",
      "url_img": "https://raw.githubusercontent.com/xinntao/HandyFigure/master/figures/template.png",
      "url_paper": "#",
-     "url_src": "https://share.weiyun.com/OPd3fja1",
+     "url_src": "https://share.weiyun.com/Kbje7Nr6",
      "url_project": "#",
  },
 {
     "title": "GFPGAN-overvie",
      "url_img": "https://raw.githubusercontent.com/xinntao/HandyFigure/master/figures/GFPGAN_overview.png",
      "url_paper": "https://arxiv.org/abs/2101.04061",
-     "url_src": "https://share.weiyun.com/Nh9MRPOM",
+     "url_src": "https://share.weiyun.com/g2GHvp4n",
      "url_project": "https://github.com/TencentARC/GFPGAN",
  },
 ];
